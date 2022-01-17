88cf1af393f63b691ab23dfa340c6a0580f46551
ardevsrc <dbeast@tuta.io>
2022-01-17 18:26:56 -0500

    Welcome app v1:
    Added app class component file

---
+++ welcome-app/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './styles.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      displayName: '', 
+      name: ''
+    }
+  }
+
+  render() {
+    return (
+      <>
+        
+      </>
+    );
+  }
+}
+
+export default App;
