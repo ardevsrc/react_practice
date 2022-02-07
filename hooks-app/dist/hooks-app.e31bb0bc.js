60ad3a6a87fcd87969b4a490e411d97f1e21eb66
ardevsrc <dbeast@tuta.io>
2022-02-07 05:28:34 -0500

    code refactored

--- hooks-app/dist/hooks-app.e31bb0bc.js
+++ hooks-app/dist/hooks-app.e31bb0bc.js
@@ -29891,7 +29891,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39739" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33331" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
