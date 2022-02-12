62ca1efaeaf5b1dea6b0e1e74fca141587d2574b
ardevsrc <dbeast@tuta.io>
2022-02-12 07:49:56 -0500

    Added footer component

--- icecream-app/src/App.js
+++ icecream-app/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Footer from './structure/Footer';
 import Header from './structure/Header';
 import './styles/ice-cream.scss';
 
 const App = () => {
   return (
-    <Header />
+    <>
+      <Header />
+      <Footer />    
+    </>
   );
 }
 
