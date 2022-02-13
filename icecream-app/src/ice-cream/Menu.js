7a06f347002b95464282701ffe08aff03a9f6f49
ardevsrc <dbeast@tuta.io>
2022-02-13 06:29:41 -0500

    Added render menu logic

--- icecream-app/src/ice-cream/Menu.js
+++ icecream-app/src/ice-cream/Menu.js
@@ -1,10 +1,50 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import {getMenu} from '../data/iceCreamData';
+import Helmet from 'react-helmet';
 
 const Menu = () => {
+  const [menu, setMenu] = useState([]);
+
+  useEffect(() => {
+    let isMounted = true;
+    getMenu().then(menuData => {
+      if (isMounted) {
+        setMenu(menuData);
+      }
+    });
+    return () => {
+      isMounted = false;
+    }
+  }, []);
+
 
   return (
     <main>
+      <Helmet>
+        <title>Rock your taste buds with one of these! | Ultimate Ice Cream</title>
+      </Helmet>
       <h2 className="main-heading">Rock your taste buds with one of these!</h2>
+      {menu.length > 0 ? 
+      (<ul className="container">
+        {menu.map(({id, iceCream, price, description, inStock, quantity}) => 
+        <li key={id.toString()}>
+          <section className="card">
+            <div className="image-container"></div>
+            <div className="text-container">
+              <h3>{iceCream.name}</h3>
+              <div className="content card-content">
+                <p className="price">{`$${price.toFixed(2)}`}</p>
+                <p className={`stock${inStock ? '' : ' out'}`}>
+                  {inStock ? `${quantity} in stock`: `Currently out of stock`}
+                </p>
+                <p className ="description">{description}</p>
+              </div>
+            </div>
+          </section>
+        </li>
+        )}
+      </ul>) : (<p> Your menu is empty! the sadness!!!</p>)
+      }
     </main>
   )
 };
