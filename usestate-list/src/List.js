d73be335c8580146b60346e23bba932315a628a3
ardevsrc <dbeast@tuta.io>
2022-03-09 19:13:18 -0500

    Added list component

---
+++ usestate-list/src/List.js
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+
+const List = () => {
+  const [people, setPeople] = useState(data);
+
+  const handleRemoveItem = (id) => {
+    setPeople(people.filter(p => p.id !== id));
+  }
+
+  return (
+    <>
+      {people.map(person => {
+        const {id, name} = person;
+        return (
+          <div key={id} className="item">
+            <h4>{name}</h4>
+            <button onClick={() => handleRemoveItem(id)}>remove</button>
+          </div>
+        );
+      })}
+
+      <button className="btn" onClick={() => setPeople([])}>clear items</button>
+    </>
+  );
+}
+
+export default List;
