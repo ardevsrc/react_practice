fc93ca02ef9c2604dc394e40e51b9ec2a7beec4c
ardevsrc <dbeast@tuta.io>
2022-03-09 19:18:47 -0500

    Imported list component

--- usestate-list/src/App.js
+++ usestate-list/src/App.js
@@ -1,9 +1,11 @@
-import React from 'react'
-
+import React from 'react';
+import List from './List';
 
 function App() {
   return (
-    <div className="container"></div>
+    <div className="container">
+      < List />
+    </div>
   );
 }
 
