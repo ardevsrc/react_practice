cec1542789d06da0f93a1a3315486ef20ed93922
ardevsrc <dbeast@tuta.io>
2022-03-12 21:32:25 -0500

    Added app component logic

--- tours/src/App.js
+++ tours/src/App.js
@@ -1,8 +1,59 @@
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Loading from './Loading';
+import Tours from './Tours';
+
+const url = 'https://course-api.com/react-tours-project';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  const fetchTours = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setIsLoading(false);
+      setTours(tours);
+    } 
+    catch (error) {
+      setIsLoading(false);
+      console.log(error);
+    }
+  }
+
+  const removeTour = (id) => {
+    const newTours = tours.filter(t => t.id !== id);
+    setTours(newTours);
+  }
+
+  if (isLoading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  if (tours.length ===0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button className="btn" onClick={fetchTours}>Refresh</button>
+        </div>
+      </main>
+    )
+  }
+
   return (
-    <h2>Project Setup</h2>
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
   );
 }
 
