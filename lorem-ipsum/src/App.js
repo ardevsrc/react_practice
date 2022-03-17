4a343f643619ac3f093eb442b550efd2d57ad4ad
ardevsrc <dbeast@tuta.io>
2022-03-16 22:47:00 -0400

    Added state management functionality

--- lorem-ipsum/src/App.js
+++ lorem-ipsum/src/App.js
@@ -5,22 +5,36 @@ function App() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState([]);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    let amount = parseInt(count, 10);
+    setText(data.slice(0, amount));
+  }
 
   return (
     <section className="section-center">
       <h3>lorem ipsum builder</h3>
-      <form className="lorem-form">
+      <form className="lorem-form" onSubmit={handleSubmit}>
         <label htmlFor="amount">paragraphs:</label>
         <input 
           type="number" 
           name="amount" 
           id="amount" 
           value={count} 
+          min={0}
+          max={20}
           onChange={(e) => setCount(e.target.value)}
         />
         <button type="submit" className="btn">generate</button>
       </form>
-
+      <article className="lorem-text">
+        {
+          text.map((item, index) => {
+            return <p key={index}>{item}</p>;
+          })
+        }
+      </article>
     </section>
   );
 }
