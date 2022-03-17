b7d994a3c8676fbbe9f7a9845676ca32a2ed80e4
ardevsrc <dbeast@tuta.io>
2022-03-16 22:09:29 -0400

    Added input form

--- lorem-ipsum/src/App.js
+++ lorem-ipsum/src/App.js
@@ -1,11 +1,27 @@
-
-import './App.css';
+import React, { useState } from 'react';
+import data from './data';
 
 function App() {
-  return (
-    <div className="App">
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
 
-    </div>
+
+  return (
+    <section className="section-center">
+      <h3>lorem ipsum builder</h3>
+      <form className="lorem-form">
+        <label htmlFor="amount">paragraphs:</label>
+        <input 
+          type="number" 
+          name="amount" 
+          id="amount" 
+          value={count} 
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit" className="btn">generate</button>
+      </form>
+
+    </section>
   );
 }
 
