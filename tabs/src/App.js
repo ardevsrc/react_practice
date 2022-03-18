dc7eb65bf8d1262388ac6241aa2e4187b6f6d367
ardevsrc <dbeast@tuta.io>
2022-03-17 23:14:16 -0400

    Added basic layout

--- tabs/src/App.js
+++ tabs/src/App.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { TransFaAngleDoubleRight } from 'react-icons/fa';
+
+const url = 'https://course-api.com/react-tabs-project';
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>experience</h2>
+        <div className="underline"></div>
+      </div>
+      <div className="btn-container">
+
+      </div>
+      <article className="job-info">
+        <h3>{title}</h3>
+        <h4>{company}</h4>
+        <p className="job-date">{dates}</p>
+      </article>
+    </section>
   );
 }
 
