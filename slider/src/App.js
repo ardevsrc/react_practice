5829f750d569d3b5e10f34ceca89e999e8cbc57d
ardevsrc <dbeast@tuta.io>
2022-03-17 23:44:33 -0400

    Added app state

--- slider/src/App.js
+++ slider/src/App.js
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
+import data from './data';
 
 
 function App() {
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = React.useState(0);
+
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(lastIndex);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people]);
+
+  useEffect(() => {
+    let slider = setInterval(() => {
+      setIndex(index + 1);
+    }, 5000);
+    return () => {
+      clearInterval(slider);
+    };
+  }, [index]);
+
+  
   return (
     <div className="App">
 
