bf8aa4f4a4c24542f913a74c5e389789b20ece87
ardevsrc <dbeast@tuta.io>
2022-03-19 00:44:08 -0400

    Added clipboard copy/paste

--- color-generator/src/SingleColor.js
+++ color-generator/src/SingleColor.js
@@ -5,10 +5,21 @@ const SingleColor = ({rgb, weight, index, hexColor}) => {
   const bcg = rgb.join(', ');
   const hexValue = `#${hexColor}`;
 
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+    return () => clearTimeout(timeout);
+  }, [alert]);
+
   return (
     <article
       className={`color ${index > 10 && 'color-light'}`}
       style={{backgroundColor: `rgb(${bcg})`}}
+      onClick={() => {
+        setAlert(true);
+        navigator.clipboard.writeText(hexValue);
+      }}
     >
       <p className='precent-value'>{weight}%</p>
       <p className='color-value'>{hexValue}</p>
