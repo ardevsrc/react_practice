9d3b3758d3df5fb82de77cfc449f64d5d78434eb
ardevsrc <dbeast@tuta.io>
2022-03-19 00:40:04 -0400

    Added single color component

---
+++ color-generator/src/SingleColor.js
@@ -0,0 +1,20 @@
+import React, {useState, useEffect} from 'react'
+
+const SingleColor = ({rgb, weight, index, hexColor}) => {
+  const [alert, setAlert] = useState(false);
+  const bcg = rgb.join(', ');
+  const hexValue = `#${hexColor}`;
+
+  return (
+    <article
+      className={`color ${index > 10 && 'color-light'}`}
+      style={{backgroundColor: `rgb(${bcg})`}}
+    >
+      <p className='precent-value'>{weight}%</p>
+      <p className='color-value'>{hexValue}</p>
+      {alert && <p className='alert'>copied to clipboard</p>}
+    </article>
+  );
+}
+
+export default SingleColor;
