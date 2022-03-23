70bbd6dcd1f66fba35440b6b66801df92b2dd817
ardevsrc <dbeast@tuta.io>
2022-03-23 19:18:59 -0400

    Added submenu component

--- stripe-navigation/src/App.js
+++ stripe-navigation/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Navbar from './Navbar';
+import Hero from './Hero';
+import Sidebar from './Sidebar';
+import Submenu from './Submenu';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <>
+      <Navbar />
+      <Sidebar />
+      <Hero />
+      <Submenu />
+    </>
   );
 }
 
--- stripe-navigation/src/Hero.js
+++ stripe-navigation/src/Hero.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import phoneImg from '../public/images/phone.svg';
+import phoneImg from './images/phone.svg';
 import { useGlobalContext } from './context';
 
 const Hero = () => {
   const { closeSubmenu } = useGlobalContext();
-
+    
   return (
-    <section className="hero">
-      <div className="hero-center">
-        <article className="hero-info">
-          <h1>Payments infrastructure for the internet</h1>
+    <section className='hero' onMouseOver={closeSubmenu}>
+      <div className='hero-center'>
+        <article className='hero-info'>
+          <h1>
+            Payments infrastructure <br />
+            for the internet
+          </h1>
           <p>
-            Million of companies of all sizes-from startups to Fortune 500s-use
-            Stripe's software and APIs to accept payments, send payouts and manage
-            their businesses online.
+            Millions of companies of all sizes—from startups to Fortune 500s—use
+            Stripe’s software and APIs to accept payments, send payouts, and
+            manage their businesses online.
           </p>
-          <button className="btn">Start now</button>
+          <button className='btn'>Start now</button>
         </article>
-        <article className="hero-images">
-          <img src={phoneImg} alt="phone" className="phone-img" />
+        <article className='hero-images'>
+          <img src={phoneImg} className='phone-img' alt='phone' />
         </article>
       </div>
     </section>
