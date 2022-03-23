170d1f6e561edf4bf9dbac3d860305952a299c44
ardevsrc <dbeast@tuta.io>
2022-03-23 10:01:52 -0400

    Added global context

---
+++ stripe-navigation/src/context.js
@@ -0,0 +1,32 @@
+import React, { useState, useContext } from 'react';
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
+
+  const openSidebar = () => setIsSidebarOpen(true);
+  const closeSidebar = () => setIsSidebarOpen(false);
+
+  const openSubmenu = () => setIsSubmenuOpen(true);
+  const closeSubmenu = () => setIsSubmenuOpen(false);
+
+  return (
+    <AppContext.Provider value= {{
+      isSidebarOpen,
+      isSubmenuOpen,
+      openSidebar,
+      closeSidebar,
+      openSubmenu,
+      closeSubmenu
+    }}>
+      {children}
+    </AppContext.Provider>
+  );
+
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+}
