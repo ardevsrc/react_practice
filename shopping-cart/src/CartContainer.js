3e22a5a4ad9a69908ba12b7997ca198df18472ae
ardevsrc <dbeast@tuta.io>
2022-03-25 14:05:26 -0400

    Added navbar component

--- shopping-cart/src/CartContainer.js
+++ shopping-cart/src/CartContainer.js
@@ -1,7 +1,18 @@
 import React from 'react'
 import CartItem from './CartItem';
+import { useGlobalContext } from './context';
 
 const CartContainer = () => {
+  const {cart, total, clearCart } = useGlobalContext();
+
+  if (cart.length === 0) {
+    return (
+      <section className="cart">
+        <h2>your bag</h2>
+        <h4 className="empty-cart">is currently empty</h4>
+      </section>
+    );
+  }
 
   return (
     <section className="cart">
@@ -9,16 +20,18 @@ const CartContainer = () => {
         <h2>your bag</h2>
       </header>
       <div>
-        {}
+        {cart.map(item => {
+          return <CartItem key={item.id} {...item} />;
+        })}
       </div>
       <footer>
         <hr />
         <div className="cart-total">
           <h4>
-            total <span>${}</span>
+            total <span>${total}</span>
           </h4>
         </div>
-        <button className="btn clear-btn">clear cart</button>
+        <button className="btn clear-btn" onClick={clearCart}>clear cart</button>
       </footer>
     </section>
   );
