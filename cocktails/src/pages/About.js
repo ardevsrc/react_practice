43c56bae116e76d3c59d6def9778b7960e42568e
ardevsrc <dbeast@tuta.io>
2022-03-26 16:42:40 -0400

    Added about page

---
+++ cocktails/src/pages/About.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Esse nesciunt voluptate assumenda soluta odit facere ipsum cum. 
+          Molestias, aliquid, mollitia est eius ab provident libero vel 
+          totam fuga magnam omnis impedit perferendis quis? 
+          Animi aspernatur totam numquam inventore, quisquam tempore.
+        </p>
+      </h1>
+    </section>
+  );
+};
+
+export default About;
+
+
