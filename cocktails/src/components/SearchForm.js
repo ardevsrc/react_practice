e34d430f7daa294efd0f021566f3db04b82da2f1
ardevsrc <dbeast@tuta.io>
2022-03-26 20:48:38 -0400

    Added searchForm component

---
+++ cocktails/src/components/SearchForm.js
@@ -0,0 +1,32 @@
+import React, { useRef, useEffect } from 'react';
+import { useGlobalContext } from '../context';
+
+const SearchForm = () => {
+  const { setSearchTerm } = useGlobalContext();
+  const searchValue = useRef('');
+
+  useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
+
+  return (
+    <section className="section search">
+      <form className="search-form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="name">Search your favorite cocktail</label>
+          <input type="text" id="name" ref={searchValue} onChange={searchCocktail}/>
+        </div>
+      </form>
+    </section>
+  )
+}
+
+export default SearchForm
