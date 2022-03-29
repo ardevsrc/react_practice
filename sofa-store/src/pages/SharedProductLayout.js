20d6264c969b019773d7986c2cf4d621c72eefe1
ardevsrc <dbeast@tuta.io>
2022-03-28 23:32:32 -0400

    Added login protected-route

--- sofa-store/src/pages/SharedProductLayout.js
+++ sofa-store/src/pages/SharedProductLayout.js
@@ -1,6 +1,4 @@
 import { Outlet } from 'react-router-dom';
-import StyledNavbar from '../components/StyledNavbar';
-
 
 const SharedLayout = () => {
   return (
@@ -8,7 +6,7 @@ const SharedLayout = () => {
       <h2>products</h2>
       <Outlet />
     </>
-  )
-}
+  );
+};
 
 export default SharedLayout;
