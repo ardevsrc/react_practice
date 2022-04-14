671e4371fdf0ec958722add7ad9d0b04cbd060fb
ardevsrc <dbeast@tuta.io>
2022-04-14 07:50:59 -0400

    Added hero component

--- comfy-sloth/src/components/Hero.js
+++ comfy-sloth/src/components/Hero.js
@@ -1,11 +1,34 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Link } from 'react-router-dom'
-import heroBcg from '../assets/hero-bcg.jpeg'
-import heroBcg2 from '../assets/hero-bcg-2.jpeg'
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import heroBcg from '../assets/hero-bcg.jpeg';
+import heroBcg2 from '../assets/hero-bcg-2.jpeg';
 
 const Hero = () => {
-  return <h4>hero</h4>
+  return (
+    <Wrapper className="section-center">
+      <article className="content">
+        <h1>design your <br />
+          comfort zone
+        </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Tempora hic beatae laudantium tenetur velit dolore quis 
+          assumenda quasi libero autem, dicta, quos ducimus impedit 
+          delectus nesciunt fuga voluptate eos est officiis possimus, 
+          ad minus labore distinctio aspernatur? Minus illum corporis 
+          et dolore maxime obcaecati quia rem, deserunt blanditiis 
+          cupiditate neque.
+        </p>
+        <Link to='/products' className='btn hero-btn'>
+          shop now
+        </Link>
+      </article>
+      <article className="img-container">
+        <img src={heroBcg} alt="nice table" className="main-img" />
+        <img src={heroBcg2} alt="person working" className="accent-img" />
+      </article>
+    </Wrapper>
+  );
 }
 
 const Wrapper = styled.section`
@@ -68,6 +91,6 @@ const Wrapper = styled.section`
       border-radius: var(--radius);
     }
   }
-`
+`;
 
-export default Hero
+export default Hero;
