3e10e7a4f2082546bbdcf6f72595c7b5258900e9
ardevsrc <dbeast@tuta.io>
2022-04-14 07:10:33 -0400

    Added about-page

--- comfy-sloth/src/pages/AboutPage.js
+++ comfy-sloth/src/pages/AboutPage.js
@@ -1,10 +1,29 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from 'react';
+import styled from 'styled-components';
+import { PageHero } from '../components';
+import aboutImg from '../assets/hero-bcg.jpeg';
 
 const AboutPage = () => {
-  return <h4>about page</h4>
+  return (
+    <main>
+      <PageHero />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="nice desk" />
+        <article>
+          <div className="title">
+            <h2>our story</h2>
+            <div className="underline"></div>
+          </div>
+          <p>Lorem ipsum dolor sit amet consectetur 
+            adipisicing elit. Ad dolore, accusantium 
+            vero itaque quae asperiores culpa maiores 
+            repudiandae reprehenderit vel sequi et 
+            laboriosam quas eveniet quidem est accusamus, odit animi?
+          </p>
+        </article>
+      </Wrapper>
+    </main>
+  );
 }
 
 const Wrapper = styled.section`
@@ -33,5 +52,6 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+
+export default AboutPage;
