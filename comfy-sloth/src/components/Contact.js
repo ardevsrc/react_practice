24b03521a51d12884f5f703b7a53a5727d6f8565
ardevsrc <dbeast@tuta.io>
2022-04-15 23:32:20 -0400

    Added home page contact

--- comfy-sloth/src/components/Contact.js
+++ comfy-sloth/src/components/Contact.js
@@ -1,8 +1,24 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
 const Contact = () => {
-  return <h4>contact section</h4>
+  return (
+    <Wrapper>
+      <div className="section-center">
+        <h3>Join our newsletter and get 20% off</h3>
+        <div className="content">
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Aut officiis recusandae, cupiditate maxime quibusdam quod 
+            unde nostrum quis amet quaerat?
+          </p>
+          <form className="contact-form">
+            <input type="email" className="form-input" placeholder="Enter email" />
+            <button type="submit" className="submit-btn">subscribe</button>
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
 }
 const Wrapper = styled.section`
   padding: 5rem 0;
