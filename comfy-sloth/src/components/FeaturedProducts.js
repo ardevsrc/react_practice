7aea6601630bcfb16ce7892b28baa8d82de61c07
ardevsrc <dbeast@tuta.io>
2022-04-16 01:03:48 -0400

    Added loading component

--- comfy-sloth/src/components/FeaturedProducts.js
+++ comfy-sloth/src/components/FeaturedProducts.js
@@ -1,12 +1,23 @@
-import React from 'react'
-import { useProductsContext } from '../context/products_context'
-import { Link } from 'react-router-dom'
-import styled from 'styled-components'
-import Error from './Error'
-import Loading from './Loading'
-import Product from './Product'
+import React from 'react';
+import { useProductsContext } from '../context/products_context';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import Error from './Error';
+import Loading from './Loading';
+import Product from './Product';
 
 const FeaturedProducts = () => {
+  const { 
+    products_loading:loading, 
+    products_error:error, 
+    featured_products:featured
+  } = useProductsContext();
+
+  if (loading) {
+    return <Loading />
+  }
+
+
   return <h4>featured products</h4>
 }
 
