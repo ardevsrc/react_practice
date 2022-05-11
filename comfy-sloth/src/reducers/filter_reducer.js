7dc93056acd9425c5a85320479bfa1a136a4c26e
ardevsrc <dbeast@tuta.io>
2022-05-10 23:07:37 -0400

    Added filter functionality

--- comfy-sloth/src/reducers/filter_reducer.js
+++ comfy-sloth/src/reducers/filter_reducer.js
@@ -72,7 +72,37 @@ const filter_reducer = (state, action) => {
   }
 
   if (action.type === FILTER_PRODUCTS) {
-    return {...state}
+    const {all_products} = state;
+    const {text, category, company, color, price, shipping} = state.filters;
+    let tempProducts = [...all_products];
+
+    if (text) {
+      tempProducts = tempProducts.filter((product) => {
+        return product.name.toLowerCase().startsWith(text);
+      })
+    }
+
+    if (category !== 'all') {
+      tempProducts = tempProducts.filter(product => product.category === category);
+    }
+
+    if (company !== 'all') {
+      tempProducts = tempProducts.filter(product => product.company === company);
+    }
+
+    if (color !== 'all') {
+      tempProducts = tempProducts.filter((product) => {
+        return product.colors.find(c => c.color === color);
+      });
+    }
+
+    if (shipping) {
+      tempProducts = tempProducts.filter(product => product.shipping === true);
+    }
+
+    tempProducts = tempProducts.filter((product) => product.price <= price);
+
+    return {...state, filtered_products: tempProducts}
   }
 
   if (action.type === CLEAR_FILTERS) {
